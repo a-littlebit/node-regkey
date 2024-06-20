dc1bc9bea2878ee98d40cbb479d8cfbf86abc3b6
vvcoder <mzp.2005@qq.com>
2024-06-20 22:58:07 +0800

    complete package.json

--- src/addon.cpp
+++ src/addon.cpp
@@ -1,15 +1,5 @@
 #include "RegKeyWrap.h"
 
-// QueryKey - Enumerates the subkeys of key and its associated values.
-//     hKey - Key whose subkeys and values are to be enumerated.
-
-#include <windows.h>
-#include <stdio.h>
-#include <tchar.h>
-
-#define MAX_KEY_LENGTH 255
-#define MAX_VALUE_NAME 16383
-
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
   RegKeyWrap::Init(env, exports);
